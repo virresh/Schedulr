9bc9bbd88d779d9c85334a16dfc45fbdcdd20146
BaaniLeen <baani16234@iiitd.ac.in>
2017-11-18 12:55:20 +0530

    Fixed error

--- Schedulr/src/constants/Constants.java
+++ Schedulr/src/constants/Constants.java
@@ -7,6 +7,8 @@ package constants;
  */
 public abstract class Constants {
 	public static int port = 8272;
+	public static int winHt = 600;
+	public static int winWt = 400;
 	public static String address = "localhost";
 	public static String[] venues = {"C01","C02","C03","C11","C12","C13","C21","C22","C23","C24","CdX","LR1","LR2","LR3","L21","L22","L23","L31","L32","L33","S01","S02"};
 	public static String[] days = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
